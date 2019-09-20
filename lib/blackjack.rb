35703c32cf31e51715425289407b0982b97530e6
Phil Billingsby <paulbillingsby@gmail.com>
2019-09-20 16:58:29 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -40,7 +40,7 @@ def hit?(card_total)
   input = get_user_input
   if get_user_input == 'h'
     card_total += deal_card
-    p card_total
+    card_total
   elsif get_user_input == "s"
     p card_total
   else
