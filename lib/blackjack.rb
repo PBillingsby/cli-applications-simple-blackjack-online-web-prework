c02e2d1ffbb30d6a56da774968486faf5ba69702
Phil Billingsby <paulbillingsby@gmail.com>
2019-09-20 16:53:47 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,22 +1,19 @@
 def welcome
-  # code #welcome here
   puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
   rand(1..11)
 end
 
-def display_card_total(card_total)
-  # code #display_card_total here
-  puts "Your cards add up to #{card_total}"
+def display_card_total(total)
+  puts "Your cards add up to #{total}"
 end
 
 def prompt_user
-  # code #prompt_user here
   puts "Type 'h' to hit or 's' to stay"
 end
+
 
 def get_user_input
   # code #get_user_input here
