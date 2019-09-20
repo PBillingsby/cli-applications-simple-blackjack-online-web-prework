e302def44971af8d5b541a1364c50d0457515dc3
Phil Billingsby <paulbillingsby@gmail.com>
2019-09-20 16:56:37 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -53,6 +53,7 @@ def invalid_command
   # code invalid_command here
   while prompt_user != ["h", "s"]
     puts "Please enter a valid command"
+  end
 end
 
 #####################################################
